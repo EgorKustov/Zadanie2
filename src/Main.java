c16a9408122548d01a87ff318818c0b52de6d125
Testik <egorkustov@icloud.com>
2023-09-27 19:18:28 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 64;
+        int priceforonemiles = 20;
+        int miles = (price / priceforonemiles);
+        System.out.println("Вам начсилили " + miles + " мили (миль)");
+
+    }
+}
